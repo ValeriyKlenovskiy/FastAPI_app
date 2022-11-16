617f90d5a408256cac44255b29d5bd5aae406346
Your Name <you@example.com>
2022-11-16 16:53:59 +0300

    third commit

--- src/workshop/api/operations.py
+++ src/workshop/api/operations.py
@@ -1,12 +1,9 @@
-from typing import List
+from typing import List, Optional
 
-from fastapi import APIRouter
-from fastapi import Depends
-from sqlalchemy.orm import Session
+from fastapi import APIRouter, Depends, Response, status
 
-from .. import tables
-from ..database import get_session
-from ..models.operations import Operation
+from ..models.operations import Operation, OperationKind, OperationCreate, OperationUpdate
+from ..services.operations import OperationService
 
 router = APIRouter(
     prefix='/operations'
@@ -14,10 +11,46 @@ router = APIRouter(
 
 
 @router.get('/', response_model=List[Operation])
-def get_operations(session: Session = Depends(get_session)):
-    operations = (
-        session
-        .query(tables.Operation)
-        .all()
+def get_operations(
+        kind: Optional[OperationKind] = None,
+        service: OperationService = Depends(),
+):
+    return service.get_list(kind=kind)
+
+
+@router.post('/', response_model=Operation)
+def create_operations(
+        operation_data: OperationCreate,
+        service: OperationService = Depends(),
+):
+    return service.create(operation_data)
+
+
+@router.get('/{operation_id}', response_model=Operation)
+def get_operation(
+        operation_id: int,
+        service: OperationService = Depends(),
+):
+    return service.get(operation_id)
+
+
+@router.put('/{operation_id}', response_model=Operation)
+def update(
+    operation_id: int,
+    operation_data: OperationUpdate,
+    service: OperationService = Depends(),
+):
+    return service.update(
+        operation_id,
+        operation_data,
     )
-    return operations
+
+
+@router.delete('/{operation_id}')
+def update(
+    operation_id: int,
+    service: OperationService = Depends(),
+):
+    service.delete(operation_id)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
