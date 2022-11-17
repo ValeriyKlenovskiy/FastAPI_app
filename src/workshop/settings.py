9dfd5a51096697336ebd03d9d8737e686c141bd3
Your Name <you@example.com>
2022-11-17 19:03:06 +0300

    fourth commit

--- src/workshop/settings.py
+++ src/workshop/settings.py
@@ -6,6 +6,11 @@ class Settings(BaseSettings):
     server_port: int = 8000
     database_url: str = 'sqlite:///./database.sqlite3'
 
+    jwt_secret: str = '2uO7qP-F33HyK0lcAFB9ZlQ3jlrizhQbrguE_YN_l-E'
+    jwt_algorithm: str = 'HS256'
+    jwt_expiration: int = 3600
+
+
 settings = Settings(
     _env_file='.env',
     _env_file_encoding='utf-8',
